11ad463660bc94891275f280da08d2d62665bea7
nova-sangeeth <novasangeeth@outlook.com>
2020-09-24 01:18:44 +0530

    added the viewsets for the notes_viewset

--- api_notes/views.py
+++ api_notes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .serializer import notes_serializer
+from .models import notes_api_model
+
+
+class notes_viewset(viewsets.ModelViewSet):
+    queryset = notes_api_model.objects.all()
+    serializer_class = notes_serializer
