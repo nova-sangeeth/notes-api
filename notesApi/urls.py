283312f3b86ba69c7e16cbcaae62695593191bbc
nova-sangeeth <novasangeeth@outlook.com>
2020-09-24 01:19:02 +0530

    added the api_notes app to the url route

--- notesApi/urls.py
+++ notesApi/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
-    path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("", include("api_notes.urls")),
+    path("admin/", admin.site.urls),
 ]
