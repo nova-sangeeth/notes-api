c5973fad4877922aa257d91cc3ee6f3fcc839c9a
nova-sangeeth <novasangeeth@outlook.com>
2020-09-23 22:08:20 +0530

    added config for allauth

--- notesApi/settings.py
+++ notesApi/settings.py
@@ -117,4 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+
+
+ACCOUNT_EMAIL_VERIFICATION = "required"
+ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True
+ACCOUNT_EMAIL_REQUIRED = False
