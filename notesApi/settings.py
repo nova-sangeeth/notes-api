0779e7360ce56834eb445eafde066b4a112fdf34
nova-sangeeth <novasangeeth@outlook.com>
2020-09-23 22:09:49 +0530

    added the required email backend config for all auth password reset.

--- notesApi/settings.py
+++ notesApi/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
