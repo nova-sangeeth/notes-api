ceb48c2c106f0ee028c4f98fbcd84b041842a138
nova-sangeeth <novasangeeth@outlook.com>
2020-09-23 23:32:25 +0530

    added new model for the notes

--- api_notes/models.py
+++ api_notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class notes_api_model(models.Model):
+    title = models.CharField(max_length=128, null=True)
+    content = models.CharField(max_length=512, null=True)
+
+    def __str__(self):
+        return self.title
