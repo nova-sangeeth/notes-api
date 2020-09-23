f5a6f73d97a218666db4e0aa524bc8c90f0d4708
nova-sangeeth <novasangeeth@outlook.com>
2020-09-24 01:15:41 +0530

    added new feilds for checking the api.

--- api_notes/models.py
+++ api_notes/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class notes_api_model(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=128, null=True)
+    author = models.CharField(max_length=128, null=True)
     content = models.CharField(max_length=512, null=True)
 
     def __str__(self):
-        return self.title
+        return str(self.user)
